9d7f9899db4896b9d39b0613208a17f9bda07946
yujing <medischdossier03@gmail.com>
2023-12-07 12:00:34 +0100

    addeventlistener ipv onclick toegevoegd

--- scripts/script.js
+++ scripts/script.js
@@ -62,18 +62,20 @@ function boeken() {
 boeken();
 
 function gridcontainer() {
-    let divresults = document.querySelectorAll(".grid-container-filter");
+    let divresults = document.querySelector(".grid-container-filter");
     let button = document.querySelector(".i--search");
 
-    button.addEventListener('click', function () {
-        divresults.forEach(function(showdiv) {
-            let displaydivresults = window.getComputedStyle(showdiv).display;/*https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle*/
+    button.addEventListener('click', () => {
+        let displaydivresults = window.getComputedStyle(divresults).display;
 
-            if (displaydivresults === "none") {/*hier staat die standaard op*/
-                showdiv.style.display = "grid";
-            }
-        });
+        if (displaydivresults === "none") {
+            divresults.style.display = "grid";
+        } else {
+            divresults.style.display = "none";
+        }
     });
+
+
 
 
 }
@@ -84,13 +86,14 @@ gridcontainer();
 function showdivresults() {/*dit staat vermoedelijk dubbel */
     let divresults = document.querySelectorAll(".showresults");
     let button = document.querySelector(".i--search");
-    button.addEventListener('click', function () {
-        divresults.forEach(function(div) {
-            let displaydivresults = window.getComputedStyle(div).display;
-            if (displaydivresults === "none") {
-                div.style.display = "grid";
-            }
-        });
+    button.addEventListener('click', () => {
+        let displaydivresults = window.getComputedStyle(divresults).display;
+
+        if (displaydivresults === "none") {
+            divresults.style.display = "grid";
+        } else {
+            divresults.style.display = "none";
+        }
     });
 
 
@@ -115,160 +118,70 @@ function filtershow() {
     let button_third = document.querySelector(".filter__collapsible-boeken-third"); // selecteer de button
 
 
-    button.onclick = () =>{
+    button.addEventListener('click', () => {
         if (formresults.style.display === "none") {
             formresults.style.display = "block"; // show the form
         } else {
             formresults.style.display = "none"; // hide the form
         }
-    }
-    button_seconds.onclick = () =>{
+    });
+    button_seconds.addEventListener('click', () => {
         if (formresults_seconds.style.display === "none") {
             formresults_seconds.style.display = "block"; // show the form
         } else {
             formresults_seconds.style.display = "none"; // hide the form
         }
-    }
-    button_third.onclick = () =>{
+    });
+    button_third.addEventListener('click', () => {
         if (formresults_third.style.display === "none") {
             formresults_third.style.display = "block"; // show the form
         } else {
             formresults_third.style.display = "none"; // hide the form
         }
-    }
+    });
 
 }
 
 filtershow();
 
 
-function searchfunction() {
-    // https://www.w3schools.com/howto/howto_js_filter_lists.asp bron waar ik code gekopieerd heb
-    let input, InputUser, listbooks, searchClassNameBook, i, titleBook, ulList, button ;
-    input = document.getElementById('form__input-searchfunciton');
+
+
+
+const searfucntion2 = () => {
+    let listbooks, ulList, button;
     button = document.querySelector('.i--search');
+    listbooks = document.querySelectorAll('.hide-li-sign');
 
-    ulList =document.querySelectorAll('.myUL')
-    listbooks = document.querySelectorAll('.hide-li-sign')
 
-    // forloop waar i het boeknummer is en alle boeken nagelopen worden
-    button.onclick = () =>{
-        InputUser = input.value.toUpperCase();//controleren hoofdletters en hier word gekeken naar de zoekopdracht
-        for (i = 0; i < ulList.length; i++) {/*dit is nodig omdat je met 2 lijsten werkt en de loop anders op 1 li werkt*/
-            for (let list = 0; list < listbooks.length; list++) {/*hier word door de list gelopen met daarin de elementen*/
+    button.addEventListener('click', () => {
+        let  inputUser = document.getElementById('form__input-searchfunciton').value.toUpperCase();
+        listbooks.forEach (li=>{
 
-                searchClassNameBook = listbooks[list].getElementsByClassName("figure-image-title-book__title-book-link")[0];/*dit moet 19 boeken zijn en alle classes zijn gekoppeld aan de li*/
-                titleBook = searchClassNameBook.textContent || searchClassNameBook.innerText;/*hier komen alle 19 boeken langs*/
-                if (titleBook.toUpperCase().indexOf(InputUser) > -1) {/*hier word gecontroleerd of de zoekopdracht overeenkomt met de titel van het booek*/
-                    listbooks[list].style.display = "";/*word het gevonden dan word het hier getoond het boek*/
-                } else {
-                    listbooks[list].style.display = "none";
+            let bookclasses = li.querySelector('.figure-image-title-book__title-book-link');
+            if (bookclasses) {
+                let titleBook = bookclasses.textContent || bookclasses.innerText;
+                if (titleBook.toUpperCase().indexOf(inputUser) > -1) {/*hier word gecontroleerd of de zoekopdracht overeenkomt met de titel van het booek*/
+                    li.style.display = "";/*word het gevonden dan word het hier getoond het boek*/
+                }
+                else {
+                    li.style.display = "none";
+                    li.style.innerHTML="helaas uw boek is niet gevonden"
                 }
             }
-        }
-        // if(notfound){
-        //     document.querySelector('.geen-resultaat').style.display='block';
-        // }
-        // else {
-        //     document.querySelector('.geen-resultaat').style.display='none';
-        // }
-        // let notfound = true;
-
-    }
+        })
+    });
     let rest = document.querySelector('.reset');
 
-    rest.onclick =()  =>{
+    rest.addEventListener('click', () => {
         for (let i = 0; i < listbooks.length; i++) {
             listbooks[i].style.display = "block"; //laat de resultaten weer zien
         }
-    }
-}
-
-// searchfunction();
-
-const searfucntion2 = () =>{
-    let  listbooks, ulList, button ;
-    button = document.querySelector('.i--search');
-
-    ulList =document.querySelectorAll('.myUL');
-    listbooks = document.querySelectorAll('.hide-li-sign');
-    let bookclasses = document.getElementsByClassName('.figure-image-title-book__title-book-link');
-    button.onclick=()=>{
-        let  inputUser = document.getElementById('form__input-searchfunciton').value.toUpperCase();
-        ulList.forEach((ul)=>{
-            console.log(`dit is de ul list${ul}`)
-            listbooks.forEach ((li)=>{
-                console.log(`dit is de ul list${li}`)
-                let titleBook = bookclasses.textContent || bookclasses.innerText;
-                if (titleBook.toUpperCase().indexOf(InputUser) > -1) {/*hier word gecontroleerd of de zoekopdracht overeenkomt met de titel van het booek*/
-                    li.style.display = "";/*word het gevonden dan word het hier getoond het boek*/
-                } else {
-                    li.style.display = "none";
-                }
-
-
-            })
-
-        })
-    }
-
+    });
 }
 searfucntion2();
 
 
-const searchfunctionarray = () =>{
-    let  listbooks , ulList, button, bookname ;
-    button = document.querySelector('.i--search');
-
-    ulList =document.querySelectorAll('.myUL');
-    listbooks = document.querySelectorAll('.hide-li-sign');
-    bookname = document.querySelectorAll('.figure-image-title-book__title-book-link');
-
-    /*omzetten nodelist naar array*/
-    let arrayUlList = Array.from(ulList);
-    let arraylistbooks = Array.from(listbooks);
-    let arraybookname = Array.from(bookname);
-    let search = [];/*lege array hier alle resultaten oplsaan*/
-    let show = '';
-
-
-    button.onclick = () => {
-        let InputUser = document.getElementById('form__input-searchfunciton').value.toUpperCase();
-
-        arrayUlList.forEach((ul) => {
-            console.log(ul);
-
-            arraylistbooks.forEach((li) => {
-                console.log(li);
-                arraybookname.forEach((book)=>{/*forlooop door de array met booknames*/
-                    let booktitle = arraybookname.textContent ||arraybookname.innerText;/*filteren resultatnen*/
-                    search.push(booktitle.toUpperCase());/*resultaten toevoegen aan search*/
-                    let FoundBooks = search.filter(title => title.includes(InputUser));/*https://developer.mozilla.org/en-US/docs/web/javascript/reference/global_objects/array/filter*/
-
-
-                    FoundBooks.forEach((found)=>{
-                        if(found){
-                         show.style.display='';
-                        }
-
-                        else{
-                            show.style.display='none';
-                        }
-                    });
-                });
-
-
-
-
-
-
-            });
-
-
-        });
-    }
-}
-searchfunctionarray();
 
 
 function allAddButtons (){
@@ -279,7 +192,7 @@ function allAddButtons (){
     let arraybooks = [];
 
         for (let buttonNumber=0; buttonNumber<buttons.length; buttonNumber++) {/*loop door deze 19 buttons*/
-            buttons[buttonNumber].onclick = () => {/*iedere keer als er op een button met een specifiek nummer is geklikt*/
+            buttons[buttonNumber].addEventListener('click', () => {/*iedere keer als er op een button met een specifiek nummer is geklikt*/
                 console.log(`er is geklikt op button:${buttonNumber}`);/*log dan de button met het specifieke nummer*/
                 display.innerHTML = (`aantal boeken aan leeslijst:${buttonNumber}`);/*dit werkt niet omdat button 0 blijft button 0 bij ajax jaarboek */
 
@@ -295,7 +208,7 @@ function allAddButtons (){
                 console.log(`dit boek is toegevoegd${arraybooks}`);
 
 
-            }
+            })
         }
 
 
@@ -316,7 +229,7 @@ let filterbuttons = () =>{
     let checkbox = document.querySelectorAll('.filter__input-checkboxes');/*https://www.w3schools.com/jsref/prop_checkbox_checked.asp*/
     let datafiltersport = document.querySelectorAll('[data-filter="sport"]');/*https://stackoverflow.com/questions/22699072/using-data-attribute-as-a-selector-to-filter-elements*/
     let dataid = document.querySelector('#onderwerp-sport,#onderwerp-cultuur,#onderwerp-kunst,#onderwerp-mode')
-    buttonfilter.addEventListener('click', function () {
+    buttonfilter.addEventListener('click', () => {
 
         // books.forEach();
 
@@ -438,3 +351,52 @@ let filterbuttons = () =>{
 // // voegtoe();
 //
 // //     https://alvarotrigo.com/blog/disable-button-javascript/
+
+
+
+
+
+// w3schools version
+//function searchfunction() {
+//     // https://www.w3schools.com/howto/howto_js_filter_lists.asp bron waar ik code gekopieerd heb
+//     let input, InputUser, listbooks, searchClassNameBook, i, titleBook, ulList, button ;
+//     input = document.getElementById('form__input-searchfunciton');
+//     button = document.querySelector('.i--search');
+//
+//     ulList =document.querySelectorAll('.myUL')
+//     listbooks = document.querySelectorAll('.hide-li-sign')
+//
+//     // forloop waar i het boeknummer is en alle boeken nagelopen worden
+//     button.onclick = () =>{
+//         InputUser = input.value.toUpperCase();//controleren hoofdletters en hier word gekeken naar de zoekopdracht
+//         for (i = 0; i < ulList.length; i++) {/*dit is nodig omdat je met 2 lijsten werkt en de loop anders op 1 li werkt*/
+//             for (let list = 0; list < listbooks.length; list++) {/*hier word door de list gelopen met daarin de elementen*/
+//
+//                 searchClassNameBook = listbooks[list].getElementsByClassName("figure-image-title-book__title-book-link")[0];/*dit moet 19 boeken zijn en alle classes zijn gekoppeld aan de li*/
+//                 titleBook = searchClassNameBook.textContent || searchClassNameBook.innerText;/*hier komen alle 19 boeken langs*/
+//                 if (titleBook.toUpperCase().indexOf(InputUser) > -1) {/*hier word gecontroleerd of de zoekopdracht overeenkomt met de titel van het booek*/
+//                     listbooks[list].style.display = "";/*word het gevonden dan word het hier getoond het boek*/
+//                 } else {
+//                     listbooks[list].style.display = "none";
+//                 }
+//             }
+//         }
+//         // if(notfound){
+//         //     document.querySelector('.geen-resultaat').style.display='block';
+//         // }
+//         // else {
+//         //     document.querySelector('.geen-resultaat').style.display='none';
+//         // }
+//         // let notfound = true;
+//
+//     }
+//     let rest = document.querySelector('.reset');
+//
+//     rest.onclick =()  =>{
+//         for (let i = 0; i < listbooks.length; i++) {
+//             listbooks[i].style.display = "block"; //laat de resultaten weer zien
+//         }
+//     }
+// }
+//
+// // searchfunction();
