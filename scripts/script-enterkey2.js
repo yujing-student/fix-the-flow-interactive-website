55005e1883f27ca28d67437ffef5081b5b639d0e
yujing <medischdossier03@gmail.com>
2023-12-19 15:05:45 +0100

    logische code volgorder erin brenngen javascript

--- scripts/script-enterkey2.js
+++ scripts/script-enterkey2.js
@@ -25,10 +25,19 @@ modeSwitch.addEventListener("click", () => {
     }
 });
 
-
 /*hierboven code amber*/
 
-const bookpage = () =>{
+bookpage();
+showFormsCheckboxes(".filter__collapsible-books", ".filter__collaps-open-function-books");
+showFormsCheckboxes(".filter__collapsible-books-second", ".filter__collaps-open-funtion-books-second");
+showFormsCheckboxes(".filter__collapsible-books-third", ".filter__collaps-open-funtion-books-third");
+showreesults();/*checkgrid zit in showresults*/
+searchBooks();
+eventsClick('.i--search','#form__input-searchfunciton');
+addList('.add-reading-list', "uw boek is toegevegd aan de leeslijst lijst\n", '.figure-image-title-book__title-book-link');
+addList('.add-reading-dvd', "uw dvd is toegevegd aan de dvd lijst\n", '.figure-image-title-book__title-book-link');
+
+function bookpage () {
     let button = document.querySelector('.button-books');
     if(button){
         button.addEventListener('click', () => {
@@ -36,36 +45,26 @@ const bookpage = () =>{
         });
     }
 }
-bookpage();
-
-// books();
-document.querySelector('#form__input-searchfunciton').addEventListener('keydown', showdivresults);
-
-function showdivresults(clickdown) {/*carousel laten zien*/
+function checkgrid (){
     let divresultsGrid = document.querySelector('.grid-container-filter');
-    let button = document.querySelector('.i--search');
-
-    if (clickdown.key === "Enter") {
-        clickdown.preventDefault();
-        divresultsGrid.style.display = 'grid';
-        divresultsGrid.scrollIntoView({
-            behavior: "smooth"
-        });
-        // divresultsGrid.style.gridArea=''
-    }
-    button.addEventListener('click', function () {
-        /*https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle*/
-        divresultsGrid.style.display = 'grid';
-        divresultsGrid.scrollIntoView({/*sroll to the queryselector*/
-            behavior: "smooth"/*https://developer.mozilla.org/en-US/docs/web/api/element/scrollintoview*/
-        });
+    divresultsGrid.style.display = 'grid';
+    divresultsGrid.scrollIntoView({/*sroll to the queryselector*/
+        behavior: "smooth"/*https://developer.mozilla.org/en-US/docs/web/api/element/scrollintoview*/
+    });
+}
+function showreesults () {
+    let buttonicon = document.querySelector('.i--search')
+    buttonicon.addEventListener('click', () => {
+        checkgrid();
     })
 
+    document.querySelector('#form__input-searchfunciton').addEventListener('keydown', (click) => {
+        if (click.key === "Enter") {
+            click.preventDefault();
+            checkgrid();
+        }
+    });
 }
-
-showdivresults('#form__input-searchfunciton');
-
-// showdivresults('.showresults','.grid-container-filter','.i--search');
 function showFormsCheckboxes(buttonSelect, formSelect) {/*use parameter*/
     // selecteer elementen
     let formCheckbox = document.querySelector(formSelect);/*formCheckbox is the ame with 3 forms */
@@ -80,83 +79,8 @@ function showFormsCheckboxes(buttonSelect, formSelect) {/*use parameter*/
     });
 }
 
-showFormsCheckboxes(".filter__collapsible-books", ".filter__collaps-open-function-books");
-showFormsCheckboxes(".filter__collapsible-books-second", ".filter__collaps-open-funtion-books-second");
-showFormsCheckboxes(".filter__collapsible-books-third", ".filter__collaps-open-funtion-books-third");
-
-
-
-
-function searfucntion(clickdown2) {
-    let button = document.querySelector('.i--search');
+function searchBooks () {
     let listbooks = document.querySelectorAll('.hide-li-sign');
-
-    if (clickdown2.key === "Enter") {
-        clickdown2.preventDefault();
-        let inputUser = document.getElementById('form__input-searchfunciton').value.toUpperCase();/*save search and keep in mind capital letters*/
-        listbooks.forEach(li => {/*for loop throug all li items with a specific class */
-            let bookclasses = li.querySelector('.figure-image-title-book__title-book-link');/*variable bookclasses with a specific class on the li*/
-            if (bookclasses) {/*checken of variabe exist*/
-                let titleBook = bookclasses.textContent || bookclasses.innerText;/*content of book or innertext save in variable titlebook*/
-                // https://developer.mozilla.org/en-US/docs/web/javascript/reference/global_objects/string/touppercase
-
-                if (titleBook.toUpperCase().indexOf(inputUser) > -1) {/*check on capitalletters and of search equal is to the title of book*/
-                    // https://developer.mozilla.org/en-US/docs/web/javascript/reference/global_objects/string/indexof
-
-                    li.style.display = "";/*empyt li because the name of book is going in the string*/
-                    li.scrollIntoView({
-                        behavior: "smooth"
-                    });
-                } else {
-                    li.style.display = "none";/*if not found display none*/
-
-                }
-            }
-        })
-
-        // divresultsGrid.style.gridArea=''
-    }
-
-    button.addEventListener('click', () => {
-        let inputUser = document.getElementById('form__input-searchfunciton').value.toUpperCase();/*save search and keep in mind capital letters*/
-        listbooks.forEach(li => {/*for loop throug all li items with a specific class */
-            let bookclasses = li.querySelector('.figure-image-title-book__title-book-link');/*variable bookclasses with a specific class on the li*/
-            if (bookclasses) {/*checken of variabe exist*/
-                let titleBook = bookclasses.textContent || bookclasses.innerText;/*content of book or innertext save in variable titlebook*/
-                // https://developer.mozilla.org/en-US/docs/web/javascript/reference/global_objects/string/touppercase
-
-                if (titleBook.toUpperCase().indexOf(inputUser) > -1) {/*check on capitalletters and of search equal is to the title of book*/
-                    // https://developer.mozilla.org/en-US/docs/web/javascript/reference/global_objects/string/indexof
-
-                    li.style.display = "";/*empyt li because the name of book is going in the string*/
-
-                } else {
-                    li.style.display = "none";/*if not found display none*/
-
-                }
-            }
-        })
-    });
-}
-
-// searfucntion('#form__input-searchfunciton');
-const eventsClick = (button,clickdown2) =>{
-    document.querySelector('#form__input-searchfunciton').addEventListener('keydown',eventsClick);
-    if (clickdown2.key === "Enter") {
-        clickdown2.preventDefault();
-        searchBooks();
-    }
-    let buttonicon = document.querySelector(button)
-    buttonicon.addEventListener('click', () => {
-        searchBooks();
-    })
-
-}
-eventsClick('.i--search','#form__input-searchfunciton')
-
-const searchBooks = () =>{
-    let listbooks = document.querySelectorAll('.hide-li-sign');
-
     let inputUser = document.getElementById('form__input-searchfunciton').value.toUpperCase();/*save search and keep in mind capital letters*/
     listbooks.forEach(li => {/*for loop throug all li items with a specific class */
         let bookclasses = li.querySelector('.figure-image-title-book__title-book-link');/*variable bookclasses with a specific class on the li*/
@@ -177,18 +101,22 @@ const searchBooks = () =>{
             }
         }
     })
-
 }
-searchBooks();
-// document.querySelector('#form__input-searchfunciton').addEventListener('keydown',search2);
+function eventsClick (button) {
+    let buttonicon = document.querySelector(button)
+    buttonicon.addEventListener('click', () => {
+        searchBooks();
+    })
 
+    document.querySelector('#form__input-searchfunciton').addEventListener('keydown', (click) => {
+        if (click.key === "Enter") {
+            click.preventDefault();
+            searchBooks();
+        }
+    });
 
-
-
-// document.querySelector('#form__input-searchfunciton').addEventListener('keydown',searfucntion);
-// https://www.geeksforgeeks.org/how-to-creating-html-list-from-javascript-array/
-
-const addList = (button, message, books) => {
+}// https://www.geeksforgeeks.org/how-to-creating-html-list-from-javascript-array/
+function addList  (button, message, books) {
     let buttons = document.querySelectorAll(button);/*select all buttons with specific class*/
     let bookTitles = document.querySelectorAll(books);/*select booktitles with specific class*/
     let displayListBooks = [];
@@ -201,7 +129,7 @@ const addList = (button, message, books) => {
             button.disabled = true;/*make button not clicalble*/
             button.textContent = message;/*show the message*/
             button.classList.add('afterclick');/*give this css proporties to the */
-            console.log(`${book}:booknummer`);/*log the number of the nodelist*/
+            // console.log(`${book}:booknummer`);/*log the number of the nodelist*/
 
             let bookTitle = bookTitles[book].textContent;/*store the content of the book in a booktitle the booktitles are 18 books and book is 1 book every time*/
             displayListBooks.push(bookTitle); // Push the title of book to the array
@@ -220,7 +148,3 @@ const addList = (button, message, books) => {
         });
     });
 }
-
-addList('.add-reading-list', "uw boek is toegevegd aan de leeslijst lijst\n", '.figure-image-title-book__title-book-link');
-addList('.add-reading-dvd', "uw dvd is toegevegd aan de dvd lijst\n", '.figure-image-title-book__title-book-link');
-/*todo de lijsten moeten nog apaart kijken naar books dat moet apart en let list*/
